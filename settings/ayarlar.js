f97fa60943aae9ec3129a69fc9884b7075396e63
helixprime1 <86311413+helixprime1@users.noreply.github.com>
2023-03-06 14:51:07 +0300

    Update ayarlar.js

--- settings/ayarlar.js
+++ settings/ayarlar.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-    TOKEN: process.env.TOKEN || "MTA3OTc2OTA0NTUyMTM5OTg0OA.Ga6Zvm.oqEDiJmNi0XK0k6yr7NKWn55EupZTcLCpDh_4E",  // bot token gir :)
+    TOKEN: process.env.TOKEN || "",  // bot token gir :)
     PREFIX: process.env.PREFIX || ".", // bot prefix
-    SAHİP: process.env.SAHİP || "291325515908775937", //sahip id :()
+    SAHİP: process.env.SAHİP || "", //sahip id :()
 }
